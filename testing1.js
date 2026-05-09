f26a2d46eef6b925443920f4915f3fcf85346bcb
NamelessOT <125128797+Namelesscode100@users.noreply.github.com>
2026-05-09 03:34:49 -0500

    Create testing2.js file

--- testing1.js
+++
@@ -1,5 +0,0 @@
-// GET_PED_MAX_HEALTH
-const retval: number = 
-	GetPedMaxHealth(
-		ped: Ped
-	);
---
+++ testing2.js
@@ -0,0 +1,4 @@
+// GET_PED_MAX_HEALTH
+const retval: number = GetPedMaxHealth(ped);
+
+console.log("Ped Max Health:", retval);
